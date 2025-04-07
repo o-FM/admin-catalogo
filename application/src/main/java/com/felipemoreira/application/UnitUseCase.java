9b732ef9c0d79726cce30e9ce715da4b5302a8bc
felipe.moreira <felipe.moreira.da.s@gmail.com>
2025-04-07 09:35:15 -0300

    Criei estrutura base para usecase

---
+++ application/src/main/java/com/felipemoreira/application/UnitUseCase.java
@@ -0,0 +1,6 @@
+package com.felipemoreira.application;
+
+public abstract class UnitUseCase<IN> {
+
+    public abstract void execute(IN in);
+}
--- application/src/main/java/com/felipemoreira/application/UseCase.java
+++ application/src/main/java/com/felipemoreira/application/UseCase.java
@@ -1,10 +1,6 @@
 package com.felipemoreira.application;
 
-import com.felipemoreira.domain.category.entity.Category;
+public abstract class UseCase<IN, OUT> {
 
-public class UseCase {
-
-    public Category execute() {
-        return null;
-    };
+    public abstract OUT execute(IN in);
 }
