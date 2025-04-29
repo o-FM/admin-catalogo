d3760c9965eac04ad70d7e78e33fc38b2ec7f3ff
felipe.moreira <felipe.moreira.da.s@gmail.com>
2025-04-29 09:02:17 -0300

    Configurei os profiles do spring

--- infrastructure/src/main/java/com/felipemoreira/infrastructure/Application.java
+++ infrastructure/src/main/java/com/felipemoreira/infrastructure/Application.java
@@ -3,11 +3,13 @@ package com.felipemoreira.infrastructure;
 import com.felipemoreira.infrastructure.config.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test-integration");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
